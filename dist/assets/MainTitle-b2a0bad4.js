2d24458ab64fad1f6d5cf8e28830851c4868dad1
Ilja Lifshic <Kriptorab608@gmail.com>
2024-10-28 23:25:41 +0200

    Initial dist subtree commit

---
+++ dist/assets/MainTitle-b2a0bad4.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as n,t as i}from"./index-744f5529.js";const o={props:{title:{type:String}}},_={class:"main-title"};function a(t,c,r,p,l,d){return s(),n("div",_,i(t.$t(this.title)),1)}const m=e(o,[["render",a]]);export{m as M};
